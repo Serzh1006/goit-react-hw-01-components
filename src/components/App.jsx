4ec882b9c23c3befd9b478aedde5fff9ff6c8875
Serzh1006 <skravchenko223@gmail.com>
2023-06-15 02:01:05 +0200

    1-task/ without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import user from '../data/user';
+import data from '../data/data';
+// import transactions from '../data/transactions';
+// import friends from '../data/friends';
+import Profile from './Profile/profile';
+import Statistics from './Statistics/statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
