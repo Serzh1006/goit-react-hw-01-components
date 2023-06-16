75765d51d52d1e6704f67876ec11082355dc18bb
Serzh1006 <skravchenko223@gmail.com>
2023-06-16 12:31:48 +0200

    change class/className

--- src/components/FriendList/friendListItem.js
+++ src/components/FriendList/friendListItem.js
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 const FriendListItem = ({ userName, isOnline, avatar }) => {
   return (
-    <li class="item">
-      <span class="status">{isOnline}</span>
-      <img class="avatar" src={avatar} alt="User avatar" width="48" />
-      <p class="name">{userName}</p>
+    <li className="item">
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{userName}</p>
     </li>
   );
 };
--- src/components/Statistics/statistics.js
+++ src/components/Statistics/statistics.js
@@ -2,25 +2,25 @@ import PropTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section class="statistics">
-      {title && <h2 class="title">{title}</h2>}
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
 
-      <ul class="stat-list">
-        <li key={stats[0].id} class="item">
-          <span class="label">{stats[0].label}</span>
-          <span class="percentage">{stats[0].percentage}%</span>
+      <ul className="stat-list">
+        <li key={stats[0].id} className="item">
+          <span className="label">{stats[0].label}</span>
+          <span className="percentage">{stats[0].percentage}%</span>
         </li>
-        <li key={stats[1].id} class="item">
-          <span class="label">{stats[1].label}</span>
-          <span class="percentage">{stats[1].percentage}%</span>
+        <li key={stats[1].id} className="item">
+          <span className="label">{stats[1].label}</span>
+          <span className="percentage">{stats[1].percentage}%</span>
         </li>
-        <li key={stats[2].id} class="item">
-          <span class="label">{stats[2].label}</span>
-          <span class="percentage">{stats[2].percentage}%</span>
+        <li key={stats[2].id} className="item">
+          <span className="label">{stats[2].label}</span>
+          <span className="percentage">{stats[2].percentage}%</span>
         </li>
-        <li key={stats[3].id} class="item">
-          <span class="label">{stats[3].label}</span>
-          <span class="percentage">{stats[3].percentage}%</span>
+        <li key={stats[3].id} className="item">
+          <span className="label">{stats[3].label}</span>
+          <span className="percentage">{stats[3].percentage}%</span>
         </li>
       </ul>
     </section>
